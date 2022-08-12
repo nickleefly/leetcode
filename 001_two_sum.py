71fdbc56619faeeb5d4b17a18ae481981e6499fe
Xiuyu Li <nickleefly@users.noreply.github.com>
2022-08-12 12:16:47 +0800

    Update 001_two_sum.py

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -1,19 +1,33 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        # two point
-        nums_index = [(v, index) for index, v in enumerate(nums)]
-        nums_index.sort()
-        begin, end = 0, len(nums) - 1
-        while begin < end:
-            curr = nums_index[begin][0] + nums_index[end][0]
-            if curr == target:
-                return [nums_index[begin][1], nums_index[end][1]]
-            elif curr < target:
-                begin += 1
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i, num in enumerate(nums):
+            if num not in hash_table:
+                hash_table[num] = [i]
             else:
-                end -= 1
+                hash_table[num].append(i)
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hash_table:
+                for j in hash_table[diff]:
+                    if i != j:
+                        return [i, j]
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i, num in enumerate(nums):
+            if num not in hash_table:
+                hash_table[num] = [i]
+            else:
+                hash_table[num].append(i)
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hash_table:
+                for j in hash_table[diff]:
+                    if i != j:
+                        return [i, j]
 
 if __name__ == '__main__':
     # begin
     s = Solution()
-    print s.twoSum([3, 2, 4], 6)
+    print(s.twoSum([3, 2, 4], 6))
