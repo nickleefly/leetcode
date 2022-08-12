0fcf6dc2b16d0210296c6976a246b744b1a773b8
Xiuyu Li <nickleefly@gmail.com>
2022-08-12 16:28:02 +0800

    add longest substring without repeating characters

---
+++ 0003-longest-substring-without-repeating-characters/main.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s is None or s == '': return 0
+        counter = 0 #max length of substring
+        start = 0 #index where all the char between the start and i are all unique
+        mark = {} #all the char index we last seen
+
+        for i in range(len(s)):
+            char_now = s[i]
+            if char_now in mark:
+                start = max(start, mark[char_now]+1)
+            counter = max(counter, i-start+1)
+            mark[char_now] = i
+
+        return counter
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print(s.lengthOfLongestSubstring("abcabcbb"))
--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@ Leetcode
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
 |1|[Two Sum](https://leetcode.com/problems/two-sum/)| [python](./0001_two_sum/main.py) |Easy|
+|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)| [python](./0002-add-two-numbers/main.py)|Medium|
+|3|[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)| [python]()|Medium|
