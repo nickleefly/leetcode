1fe315a8c16cdb1b10f25b7ccdca3e4645d54ae4
Xiuyu Li <nickleefly@users.noreply.github.com>
2022-08-12 12:02:11 +0800

    Create 001_two_sum.py

---
+++ 001_two_sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        # two point
+        nums_index = [(v, index) for index, v in enumerate(nums)]
+        nums_index.sort()
+        begin, end = 0, len(nums) - 1
+        while begin < end:
+            curr = nums_index[begin][0] + nums_index[end][0]
+            if curr == target:
+                return [nums_index[begin][1], nums_index[end][1]]
+            elif curr < target:
+                begin += 1
+            else:
+                end -= 1
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print s.twoSum([3, 2, 4], 6)
