93693a653c5054486a84a5ff24e5a80f4e558ecf
Xiuyu Li <nickleefly@gmail.com>
2022-08-25 16:02:14 +0800

    update

--- 001-two-sum/main.py
+++ 001-two-sum/main.py
@@ -1,33 +1,24 @@
 from typing import List
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_table = {}
-        for i, num in enumerate(nums):
-            if num not in hash_table:
-                hash_table[num] = [i]
-            else:
-                hash_table[num].append(i)
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hash_table:
-                for j in hash_table[diff]:
-                    if i != j:
-                        return [i, j]
-    def twoSum1(self, nums: List[int], target: int) -> List[int]:
-        hash_table = {}
-        for i, num in enumerate(nums):
-            if num not in hash_table:
-                hash_table[num] = [i]
-            else:
-                hash_table[num].append(i)
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hash_table:
-                for j in hash_table[diff]:
-                    if i != j:
-                        return [i, j]
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        {
+            3: 0,
+            2: 1,
+            7: 2,
+            4: 3
+        }
+
+        """
+        hashtable = dict()
+        for i, num in enumerate(numbers):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[num] = i
+
+        return []
 
 if __name__ == '__main__':
     # begin
     s = Solution()
-    print(s.twoSum([3, 2, 4], 6))
+    print(s.twoSum([3, 2, 7, 4], 6))
