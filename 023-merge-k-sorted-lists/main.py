a53261ffc7e5850f42929c911b6f49f8cf40fe37
Xiuyu Li <nickleefly@gmail.com>
2022-08-31 14:49:08 +0800

    add 23 merge k sorted lists

---
+++ 023-merge-k-sorted-lists/main.py
@@ -0,0 +1,177 @@
+from typing import List
+from typing import Optional
+from operator import attrgetter
+import heapq
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    # def __repr__(self):
+    #     if self:
+    #         return "{} -> {}".format(self.val, self.next)
+
+class Solution:
+    # @param a list of ListNode
+    # @return a ListNode
+    # Time: O(nlogn) Space: O(1)
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+
+        return self.merge_range_lists(lists, 0, len(lists) - 1)
+
+    def merge_range_lists(self, lists, start, end):
+        if start == end:
+            return lists[start]
+
+        mid = (start + end) // 2
+        left = self.merge_range_lists(lists, start, mid)
+        right = self.merge_range_lists(lists, mid + 1, end)
+        return self.merge_two_lists(left, right)
+
+    def merge_two_lists(self, head1, head2):
+        tail = dummy = ListNode(0)
+        while head1 and head2:
+            if head1.val < head2.val:
+                tail.next = head1
+                head1 = head1.next
+            else:
+                tail.next = head2
+                head2 = head2.next
+            tail = tail.next
+
+        if head1:
+            tail.next = head1
+        if head2:
+            tail.next = head2
+
+        return dummy.next
+
+    def mergeKLists1(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        sorted_list = []
+        for head in lists:
+            curr = head
+            while curr is not None:
+                sorted_list.append(curr)
+                curr = curr.next
+
+        sorted_list = sorted(sorted_list, key=attrgetter('val'))
+        for i, node in enumerate(sorted_list):
+            try:
+                node.next = sorted_list[i + 1]
+            except:
+                node.next = None
+
+        if sorted_list:
+            return sorted_list[0]
+        else:
+            return None
+
+    # Building the new list with just the values from the old ones, 
+    # leaving the old lists intact
+    def mergeKLists2(self, lists):
+        def vals(node):
+            while node:
+                yield node.val
+                node = node.next
+        dummy = last = ListNode(None)
+        for val in heapq.merge(*map(vals, lists)):
+            last.next = last = ListNode(val)
+        return dummy.next
+
+    # Building the new list with the nodes from the old ones
+    def mergeKLists3(self, lists):
+        def gen(node):
+            while node:
+                yield node.val, node
+                node = node.next
+        dummy = last = ListNode(None)
+        for _, last.next in heapq.merge(*map(gen, lists)):
+            last = last.next
+        return dummy.next
+
+    # def mergeKLists_heap(self, lists: List[ListNode]) -> ListNode:
+    """
+    1, Take the first node of each of the linked lists
+    and add it into a heap. When you add it to the heap
+    add (node.val, i) where i is the ith list.
+
+    2, Create a dummy node head.
+
+    3, Pop the first node from the heap and make it the
+    next node in the dummy-list. Remember to add the
+    first node from the ith linked list into the heap
+    since we just removed a node from this list from the heap.
+
+    4, Repeat until the heap is empty.
+    Time Complexity: O(n·log(m)) where n is the total number of elements and m is the number of lists
+    Space Complexity: O(n)
+    """
+    def mergeKLists_heap(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        head = ListNode(None)
+        curr = head
+        h = []
+        for i in range(len(lists)):
+            # print('list[%d] is %s' % (i, lists[i].val))
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+
+        while h:
+            val, i = heapq.heappop(h)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+
+        return head.next
+
+    def mergeKLists_Python3(self, lists):
+        ListNode.__eq__ = lambda self, other: self.val == other.val
+        ListNode.__lt__ = lambda self, other: self.val < other.val
+        h = []
+        head = tail = ListNode(0)
+        for i in lists:
+            if i:
+                heapq.heappush(h, (i.val, i))
+
+        while h:
+            node = heapq.heappop(h)[1]
+            tail.next = node
+            tail = tail.next
+            if node.next:
+                heapq.heappush(h, (node.next.val, node.next))
+
+        return head.next
+
+    def mergeKLists_heapq1(self, lists):
+        h = []
+        head = tail = ListNode(0)
+        for i in range(len(lists)):
+            heapq.heappush(h, (lists[i].val, i, lists[i]))
+
+        while h:
+            node = heapq.heappop(h)
+            node = node[2]
+            tail.next = node
+            tail = tail.next
+            if node.next:
+                i+=1
+                heapq.heappush(h, (node.next.val, i, node.next))
+
+        return head.next
+
+if __name__ == "__main__":
+    list1 = ListNode(1)
+    list1.next = ListNode(4)
+    list1.next.next = ListNode(5)
+    list2 = ListNode(1)
+    list2.next = ListNode(3)
+    list2.next.next = ListNode(4)
+    list3 = ListNode(2)
+    list3.next = ListNode(6)
+    # print(Solution().mergeKLists1([list1, list2, list3]))
+    print(Solution().mergeKLists_heapq1([list1, list2, list3]))
--- readme.md
+++ readme.md
@@ -1,8 +1,26 @@
 Leetcode
 ===========
 
+| # | Title | Solution | Difficulty | Time | Space |
+|---| ----- | -------- | ---------- | ---- | ----- |
+|1|[Two Sum](https://leetcode.com/problems/two-sum/)|[python](./001-two-sum/main.py)|Easy|O(n)|O(n)|
+|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|[python](./002-add-two-numbers/main.py)|Medium|O(n)|O(n)|
+|3|[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)|[python](./003-longest-substring-without-repeating-characters/main.py)|Medium|O(n)|O(1)|
+|4|[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/)|[python](./004-median-of-two-sorted-arrays/main.py)|Hard|O(log(min(m, n)))|O(1)|
+|23|[Merge K Sorted Lists](https://leetcode.com/problems/merge-k-sorted-lists/)|[python](./023-merge-k-sorted-lists/main.py)|Hard|O(nlogk)|O(1)|
+|53|[Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)|[python](./053-maximum-subarray/main.py)|Medium|O(n)|O(1)|
+|97|[Interleaving String](https://leetcode.com/problems/interleaving-string/)|[python](./097-interleaving-string/main.py)|Hard|O(m * n)|O(m + n)|
+|124|[Binary Tree Maximum Path Sum](https://leetcode.com/problems/binary-tree-maximum-path-sum/)|[python](./124-binary-tree-maximum-path-sum/main.py)|Hard|O(n)|O(n)|
+|141|[Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/)|[python](./141-linked-list-cycle/main.py)|Easy|O(n)|O(1)|
+|264|[Ugly Number II](https://leetcode.com/problems/ugly-number-ii/)|[python](./264-ugly-number-ii/main.py)|Medium|O(n)|O(1)|
+|300|[Longest Increasing Subsequence](https://leetcode.com/problems/longest-increasing-subsequence/)|[python](./300-longest-increasing-subsequence/main.py)|Medium|O(nlogn)|O(n)|
+|589|[N-ary Tree Preorder Traversal](https://leetcode.com/problems/n-ary-tree-preorder-traversal/)|[python](./589-n-ary-tree-preorder-traversal/main.py)|Easy|||
+|1835|[Number of Ways to Stay in the Same Place After Some Steps I](https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/)|Easy|||
+
+Lintcode
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
-|1|[Two Sum](https://leetcode.com/problems/two-sum/)| [python](./001-two-sum/main.py) |Easy|
-|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)| [python](./002-add-two-numbers/main.py)|Medium|
-|3|[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)| [python](./003-longest-substring-without-repeating-characters/main.py)|Medium|
+|42|[Maximum Subarray II](https://www.lintcode.com/problem/42)|[python](./maximum-subarray-ii/main.py)|Medium
+|45|[Maximum Subarray Difference](https://www.lintcode.com/problem/45)|[python](./maxium-subarray-difference/main.py)|Medium|
+|366|[Fibonacci](https://www.lintcode.com/problem/366/)|[python](./fibonacci/main.py)|Naive|
+
