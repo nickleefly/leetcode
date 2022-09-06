9e6622ad71f406eeacfe8fd1b9109dae1177a8df
Xiuyu Li <nickleefly@gmail.com>
2022-09-06 16:28:09 +0800

    add comment for valid parentheses

--- valid-parentheses/main.py
+++ valid-parentheses/main.py
@@ -1,3 +1,7 @@
+"""
+Time: O(n)
+Space: O(n)
+"""
 class Solution:
     def is_valid_parentheses(self, s: str) -> bool:
         if len(s) % 2 == 1:
@@ -12,6 +16,10 @@ class Solution:
         for ch in s:
             # print('\n')
             # print("ch is %s, ch in pairs %s" % (ch, ch in pairs))
+
+            # when its right parentheses, we check if top of stack matching with current
+            # if not matching return False
+            # if matching we pop out left parentheses from stack
             if ch in pairs:
                 # print('stack is ', stack)
                 # print('stack[-1] is %s pairs["%s"] is %s' % (stack[-1], ch, pairs[ch]))
@@ -22,10 +30,24 @@ class Solution:
                 stack.pop()
                 # print('stack after pop', stack)
             else:
+                # we put left parentheses in stack
                 stack.append(ch)
                 # print('stack after append', stack)
         return not stack
 
+    """
+
+    """
+    def is_valid_parentheses2(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            # left parentheses put in to stack
+            if c in dic: stack.append(c)
+            # if not, we check if matching closing pair in dict
+            elif dic[stack.pop()] != c: return False
+        return len(stack) == 1
+
 s = Solution()
 s_input = "()[]{}"
-print(s.is_valid_parentheses(s_input))
+print(s.is_valid_parentheses2(s_input))
